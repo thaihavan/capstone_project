74009ad582ac228c207c1b19be9b881c70e6132d
PhongTV97 <phongvpt1997@gmail.com>
2019-05-14 22:52:34 +0700

    Add demo unit test

---
+++ study/Unit test/DemoUnitTest/DemoUnitTest/Functions.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DemoUnitTest
+{
+    public class Functions
+    {
+        public int add(int a, int b)
+        {
+            return a + b;
+        }
+        public bool checkOdd(int a)
+        {
+            if (a % 2 == 1)
+                return true;
+            return false;
+        }
+
+        public int MyRandom()
+        {
+            return new Random().Next(0, 10);
+        }
+
+        public virtual int SquareOfRandom()
+        {
+            int result = MyRandom();
+
+            result *= result;
+
+            return result;
+
+        }
+
+    }
+}
---
+++ study/Unit test/DemoUnitTest/MyUnitTest/MyTest.cs
@@ -0,0 +1,52 @@
+﻿using Moq;
+using NUnit.Framework;
+using System;
+
+namespace MyUnitTest
+{
+    [TestFixture]
+    public class MyTest
+    {
+        Mock myInterfaceMock;
+
+     
+        [TestCase]
+        public void TestAddisTrue()
+        {
+            var func = new DemoUnitTest.Functions();
+            int resultExpected = 11;
+            int resultFunc = func.add(5, 6);
+            Assert.AreEqual(resultExpected, resultFunc);
+        }
+
+        [TestCase]
+        public void TestAddisFalse()
+        {
+            var func = new DemoUnitTest.Functions();
+            int resultExpected = 6;
+            int resultFunc = func.add(5, 6);
+            Assert.AreEqual(resultExpected, resultFunc);
+        }
+
+        [TestCase]
+        public void TestCheckOdd()
+        {
+            var func = new DemoUnitTest.Functions();
+            bool resultFunc = func.checkOdd(6);
+            Assert.IsTrue(resultFunc);
+        }
+
+        [TestCase]
+        public void DemoMoq()
+        {
+            int numberToReturn = 4;
+            Mock< DemoUnitTest.Functions> moqResult = new Mock<DemoUnitTest.Functions> ();
+            moqResult.CallBase = true;
+            moqResult.Setup(x => x.SquareOfRandom()).Returns(numberToReturn);
+
+            var results = moqResult.Object.SquareOfRandom();
+
+            Assert.AreEqual(4, results);
+        }
+    }
+}
