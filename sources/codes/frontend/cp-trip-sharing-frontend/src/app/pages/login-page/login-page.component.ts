29402ae49ffa46811c7564bdfdbde1350c8ee734
PhongTV97 <phongvpt1997@gmail.com>
2019-05-18 21:11:06 +0700

    add login-page component

--- sources/codes/frontend/cp-trip-sharing-frontend/src/app/pages/login-page/login-page.component.ts
+++ sources/codes/frontend/cp-trip-sharing-frontend/src/app/pages/login-page/login-page.component.ts
@@ -6,10 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login-page.component.css']
 })
 export class LoginPageComponent implements OnInit {
+  showformLogin = false;
+  showformLoginFB = true;
 
   constructor() { }
 
   ngOnInit() {
   }
+  changePage(){
+    if(this.showformLogin == true && this.showformLoginFB == false ){
+      this.showformLogin=false;
+      this.showformLoginFB = true;
+    }
+    else{
+      this.showformLogin=true;
+      this.showformLoginFB = false;
+    }
+    
+  }
+
 
 }
