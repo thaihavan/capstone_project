eb11dd452b8b1f5d7ef2a83b782c83204756ae8e
PhongTV97 <phongvpt1997@gmail.com>
2019-06-11 10:56:09 +0700

    add interestedtopic component

---
+++ sources/codes/frontend/cp-trip-sharing-frontend/src/app/pages/interestedtopic-page/interestedtopic-page.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+@Component({
+  selector: 'app-interestedtopic-page',
+  templateUrl: './interestedtopic-page.component.html',
+  styleUrls: ['./interestedtopic-page.component.css']
+})
+export class InterestedtopicPageComponent implements OnInit {
+   listinterestedtopic: Topic[]=[
+     {nameTopic:"Văn Hóa",urlImage: "https://gody.vn/public/v3/images/bg/br-register.jpg"},
+     {nameTopic:"Văn Hóa",urlImage: "https://gody.vn/public/v3/images/bg/br-register.jpg"},
+     {nameTopic:"Văn Hóa",urlImage: "https://gody.vn/public/v3/images/bg/br-register.jpg"},
+     {nameTopic:"Văn Hóa",urlImage: "https://gody.vn/public/v3/images/bg/br-register.jpg"},
+     {nameTopic:"Văn Hóa",urlImage: "https://gody.vn/public/v3/images/bg/br-register.jpg"},
+     {nameTopic:"Văn Hóa",urlImage: "https://gody.vn/public/v3/images/bg/br-register.jpg"}
+    ];
+
+    nameclass:string ="eachtopic";
+  constructor() { }
+
+  ngOnInit() {
+  }
+  
+  changeClass(): void{
+    this.nameclass="changeClass";
+  }
+}
+
+export class Topic{
+  nameTopic: string;
+  urlImage: string;
+}
