d6130f0ac2d37144164a90acc010e7d910de2a94
Truong Le <xuantruonga3k8@gmail.com>
2019-06-14 19:06:11 +0700

    Update UserService

--- sources/codes/backend/cp-trip-sharing-backend/src/Services/UserService/UserService/Controllers/UserServiceController.cs
+++ sources/codes/backend/cp-trip-sharing-backend/src/Services/UserService/UserService/Controllers/UserServiceController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using MongoDB.Bson;
@@ -23,12 +24,28 @@ namespace UserServices.Controllers
             _userService = userService;
         }
 
-        //just test
-        [HttpGet("all")]
-        public IActionResult GetAll()
+        [Authorize(Roles = "member")]
+        [HttpPost("Register")]
+        public IActionResult Register([FromBody] User userParam)
         {
-            var follows = _userService.GetAll();
-            return new OkObjectResult(follows);
+            var result = _userService.Add(userParam);
+            return Created("", userParam);
+        }
+
+        [Authorize(Roles = "admin")]
+        [HttpGet("All")]
+        public IActionResult GetAll([FromBody] User userParam)
+        {
+            var result = _userService.GetAll();
+            return Ok(result);
+        }
+
+        [AllowAnonymous]
+        [HttpGet("User")]
+        public IActionResult GetUserById([FromQuery] string userId)
+        {
+            var result = _userService.
+            return Ok(result);
         }
 
         // POST: api/UserServices/follow
