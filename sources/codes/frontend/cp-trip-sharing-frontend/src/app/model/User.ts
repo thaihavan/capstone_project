d93a5a2f55961acb7b71295a0cf0645c3ff818a7
PhongTV97 <phongvpt1997@gmail.com>
2019-06-18 10:49:06 +0700

    update User.ts

---
+++ sources/codes/frontend/cp-trip-sharing-frontend/src/app/model/User.ts
@@ -0,0 +1,9 @@
+export class User {
+    public UserId: string;
+    public FirstName: string;
+    public LastName: string;
+    public DisplayName: string;
+    public DOB: Date;
+    public Interested: string[];
+    public Gender: boolean;
+}
