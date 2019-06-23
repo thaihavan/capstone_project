0bd837e3a4ce4dbae63673e8a375ccbb569ffe47
Truong Le <xuantruonga3k8@gmail.com>
2019-06-23 16:33:39 +0700

    Update Test

--- sources/codes/backend/cp-trip-sharing-backend/src/Services/PostService/PostService.Test/LikeServiceTest.cs
+++ sources/codes/backend/cp-trip-sharing-backend/src/Services/PostService/PostService.Test/LikeServiceTest.cs
@@ -4,8 +4,8 @@ using Moq;
 using NUnit.Framework;
 using PostService.Helpers;
 using PostService.Models;
-using PostService.Repositories;
 using PostService.Repositories.Interfaces;
+using PostService.Services;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -13,10 +13,10 @@ using System.Text;
 namespace PostService.Test
 {
     [TestFixture]
-    class LikeRepositoryTest
+    public class LikeServiceTest
     {
         AppSettings _settings;
-        LikeRepository _likeRepository = null;
+        Mock<ILikeRepository> mockLikeService;
 
         [SetUp]
         public void Config()
@@ -27,29 +27,34 @@ namespace PostService.Test
                 ConnectionString = "mongodb://tripsharing:tripsharing@cluster0-shard-00-00-vkzdk.gcp.mongodb.net:27017,cluster0-shard-00-01-vkzdk.gcp.mongodb.net:27017,cluster0-shard-00-02-vkzdk.gcp.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority",
                 DatabaseName = "TripSharing-PostService"
             };
-            _likeRepository = new LikeRepository(Options.Create(_settings));
+            mockLikeService = new Mock<ILikeRepository>();
         }
 
         [TestCase]
-        public void TestAddLike()
+        public void TestAdd()
         {
             Like like = new Like()
             {
-                ObjectId ="5d027ea59b358d247cd21a55",
-                ObjectType = "comment",
-                UserId = "5d04cc646c9cec228cf9bb09",
+                ObjectId = "5d027ea59b358d247cd21a55",
+                ObjectType = "post",
+                UserId = "5d027ea59b358d247cd21a54",
                 Date = DateTime.Now
             };
-            Like testLike = _likeRepository.Add(like);
-            Assert.AreEqual(testLike, like);
+            mockLikeService.Setup(x => x.Add(It.IsAny<Like>())).Returns(like);
+            var testService = new LikeService(Options.Create(_settings));
+            Like result = testService.Add(like);
+            Assert.AreEqual(like, result);
         }
 
-        [TestCase]
-        public void TestDelete()
-        {
-            string postId = "5d027ea59b358d247cd21a55";
-            bool like = _likeRepository.Delete(postId);
-            Assert.AreEqual(true, like);
-        }
+        //[TestCase]
+        //public void TestDelete()
+        //{
+        //    string userId = "5d027ea59b358d247cd21a55";
+        //    string objectId = "5d027ea59b358d247cd21a55";
+        //    mockLikeService.Setup(x => x.Delete(It.IsAny<string>(), It.IsAny<string>())).Returns(true);
+        //    var testService = new LikeService(Options.Create(_settings));
+        //    bool result = testService.Delete(objectId, userId)
+        //}
+
     }
 }
