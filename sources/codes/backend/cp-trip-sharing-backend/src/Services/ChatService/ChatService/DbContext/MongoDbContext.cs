5109617b497b0cb870dda78758595126ecab0606
Truong Le <xuantruonga3k8@gmail.com>
2019-06-24 23:46:28 +0700

    Add ChatSerVice
    Not complete

---
+++ sources/codes/backend/cp-trip-sharing-backend/src/Services/ChatService/ChatService/DbContext/MongoDbContext.cs
@@ -0,0 +1,49 @@
+﻿using ChatService.Helpers;
+using ChatService.Models;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChatService.DbContext
+{
+    public class MongoDbContext
+    {
+        private readonly IMongoDatabase _database = null;
+
+        public MongoDbContext(IOptions<AppSettings> settings)
+        {
+            var mongoClient = new MongoClient(settings.Value.ConnectionString);
+            if (mongoClient != null)
+            {
+                _database = mongoClient.GetDatabase(settings.Value.DatabaseName);
+            }
+        }
+
+        public IMongoCollection<Conversation> Conversations
+        {
+            get
+            {
+                if (_database != null)
+                {
+                    return _database.GetCollection<Conversation>("Conversations");
+                }
+                return null;
+            }
+        }
+
+        public IMongoCollection<User> Users
+        {
+            get
+            {
+                if (_database != null)
+                {
+                    return _database.GetCollection<User>("Users");
+                }
+                return null;
+            }
+        }
+    }
+}
---
+++ sources/codes/backend/cp-trip-sharing-backend/src/Services/ChatService/ChatService/Models/Conversation.cs
@@ -0,0 +1,19 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChatService.Models
+{
+    public class Conversation
+    {
+        [BsonId]
+        [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [BsonRepresentation(MongoDB.Bson.BsonType.Array)]
+        public List<string> ListUserId { get; set; }
+
+    }
+}
