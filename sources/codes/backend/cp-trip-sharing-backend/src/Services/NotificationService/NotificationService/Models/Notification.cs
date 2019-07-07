9cdc95f5c8c7bc1ac8a9c1adbfea5e1e0292a137
linhlp <linhlpse04693@gmail.com>
2019-07-07 23:12:00 +0700

    added notification service + updated article repository

---
+++ sources/codes/backend/cp-trip-sharing-backend/src/Services/NotificationService/NotificationService/Models/Notification.cs
@@ -0,0 +1,30 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NotificationService.Models
+{
+    public class Notification:Model
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        [BsonElement("_id")]
+        public string Id { get; set; }
+
+        [BsonRepresentation(BsonType.ObjectId)]
+        [BsonElement("receiver")]
+        public string Receiver { get; set; }
+
+        [BsonElement("seen")]
+        public bool Seen { get; set; }
+        
+        [BsonElement("date")]
+        public DateTime Date { get; set; }
+
+
+
+    }
+}
