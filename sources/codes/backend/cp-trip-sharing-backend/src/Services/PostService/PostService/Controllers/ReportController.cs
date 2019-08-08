42b4d3f84b3213502c689be67de5d5be51415898
linhlp <linhlpse04693@gmail.com>
2019-08-08 21:56:52 +0700

    added report to post service
    Add new report
    get all report for all kind of post and comment

---
+++ sources/codes/backend/cp-trip-sharing-backend/src/Services/PostService/PostService/Controllers/ReportController.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Bson;
+using PostService.Models;
+using PostService.Services.Interfaces;
+
+namespace PostService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReportController : ControllerBase
+    {
+        private readonly IReportService _reportService = null;
+
+        public ReportController(IReportService reportService)
+        {
+            _reportService = reportService;
+        }
+
+        [Authorize(Roles ="member")]
+        [HttpPost]
+        public IActionResult AddNewReport([FromBody] Report param)
+        {
+            if (param == null)
+            {
+                return BadRequest();
+            }
+            var identity = (ClaimsIdentity)User.Identity;
+            var userId = identity.FindFirst("user_id").Value;
+
+            param.Id = ObjectId.GenerateNewId().ToString();
+            param.Date = DateTime.Now;
+            param.ReporterId = userId;
+            return Ok(_reportService.Add(param));
+        }
+
+        [Authorize(Roles = "admin")]
+        [HttpDelete()]
+        public IActionResult DeleteAReport([FromQuery] string id)
+        {
+            return Ok(_reportService.Delete(id));
+        }
+
+        [Authorize(Roles = "member")]
+        [HttpGet("type")]
+        public IActionResult GetAllReportType()
+        {
+            return Ok(_reportService.GetAllReportType());
+        }
+
+        [Authorize(Roles ="admin")]
+        [HttpGet("article")]
+        public IActionResult GetAllArticleReports([FromQuery] int page)
+        {
+            return Ok(_reportService.GetAllArticleReports(page));
+        }
+
+        [Authorize(Roles = "admin")]
+        [HttpGet("comment")]
+        public IActionResult GetAllCommentReports([FromQuery] int page)
+        {
+            return Ok(_reportService.GetAllCommentReports(page));
+        }
+
+        [Authorize(Roles = "admin")]
+        [HttpGet("companion-post")]
+        public IActionResult GetAllCompanionPostReports([FromQuery] int page)
+        {
+            return Ok(_reportService.GetAllCompanionPostReports(page));
+        }
+
+        [Authorize(Roles = "admin")]
+        [HttpGet("virtual-trip")]
+        public IActionResult GetAllVirtualTripReports([FromQuery] int page)
+        {
+            return Ok(_reportService.GetAllVirtualTripReports(page));
+        }
+    }
+}
---
+++ sources/codes/backend/cp-trip-sharing-backend/src/Services/PostService/PostService/Services/ReportService.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Options;
+using PostService.Helpers;
+using PostService.Models;
+using PostService.Repositories;
+using PostService.Repositories.Interfaces;
+using PostService.Services.Interfaces;
+
+namespace PostService.Services
+{
+    public class ReportService : IReportService
+    {
+        private readonly IReportRepository _reportRepository = null;
+
+        public ReportService(IOptions<AppSettings> settings)
+        {
+            _reportRepository = new ReportRepository(settings);
+        }
+
+        public Report Add(Report param)
+        {
+            return _reportRepository.Add(param);
+        }
+
+        public bool Delete(string id)
+        {
+            return _reportRepository.Delete(id);
+        }
+
+        public IEnumerable<Report> GetAllArticleReports(int page)
+        {
+            return _reportRepository.GetAllArticleReports(page);
+        }
+
+        public IEnumerable<Report> GetAllCommentReports(int page)
+        {
+            return _reportRepository.GetAllCommentReports(page);
+        }
+
+        public IEnumerable<Report> GetAllCompanionPostReports(int page)
+        {
+            return _reportRepository.GetAllCompanionPostReports(page);
+        }
+
+        public IEnumerable<ReportType> GetAllReportType()
+        {
+            return _reportRepository.GetAllReportType();
+        }
+
+        public IEnumerable<Report> GetAllVirtualTripReports(int page)
+        {
+            return _reportRepository.GetAllVirtualTripReports(page);
+        }
+    }
+}
